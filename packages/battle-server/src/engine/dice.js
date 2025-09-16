9924f0394d8330653121a4090ca9b5a0cda7c98f
CriminalTalent <rntjdrud@naver.com>
2025-09-16 22:44:18 +0900

    Update dice.js

--- packages/battle-server/src/engine/dice.js
+++ packages/battle-server/src/engine/dice.js
@@ -1,4 +1,5 @@
 // packages/battle-server/src/engine/dice.js
 export const rndInt = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min);
-export const d20 = () => rndInt(1, 20);
+export const d20 = () => rndInt(1, 10); // D20 -> D10으로 변경
+export const d10 = () => rndInt(1, 10); // D10 함수 추가
 export const d = (n) => rndInt(1, n);
